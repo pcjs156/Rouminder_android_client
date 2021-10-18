8b9bc95071d9e56609a7e92b868848e9f2b0c951
윤민지 <gee2450@kookmin.ac.kr>
2021-10-19 02:15:39 +0900

    Add goal, profile, statistics fragments, and bottom Navigation MainActivity.java에 bottom Navigation과 fragment 전환 구현

--- app/src/main/java/com/example/rouminder/LoginActivity.java
+++ app/src/main/java/com/example/rouminder/LoginActivity.java
@@ -2,13 +2,32 @@ package com.example.rouminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnlogin = (Button) findViewById(R.id.btnLogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "로그인", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
     }
 }
