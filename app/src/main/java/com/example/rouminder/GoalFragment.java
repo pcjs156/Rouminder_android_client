e2ec9ca50eef85d2a6e46b5068784e5009f0e95c
윤민지 <gee2450@kookmin.ac.kr>
2021-10-19 16:23:28 +0900

    Add switching calendar function build에 dependencies 추가 fragment_goal.xml, GoalFragment.java 수정

--- app/src/main/java/com/example/rouminder/GoalFragment.java
+++ app/src/main/java/com/example/rouminder/GoalFragment.java
@@ -3,17 +3,26 @@ package com.example.rouminder;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.nex3z.togglebuttongroup.button.CircularToggle;
 
 public class GoalFragment extends Fragment {
 
     ImageView btnAddGoal;
+
+    CircularToggle choiceDay;
+    CircularToggle choiceWeek;
+    CircularToggle choiceMonth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +31,14 @@ public class GoalFragment extends Fragment {
 
         btnAddGoal = (ImageView) rootView.findViewById(R.id.btnAddGoal);
 
+        choiceDay = (CircularToggle) rootView.findViewById(R.id.choiceDay);
+        choiceWeek = (CircularToggle) rootView.findViewById(R.id.choiceWeek);
+        choiceMonth = (CircularToggle) rootView.findViewById(R.id.choiceMonth);
+
+
+        LinearLayout weeklyCalendar = (LinearLayout) rootView.findViewById(R.id.weeklyCalendar);
+        CardView monthlyCalendar = (CardView) rootView.findViewById(R.id.monthlyCalendar);
+
         btnAddGoal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +46,36 @@ public class GoalFragment extends Fragment {
 
                 Intent intent = new Intent(getActivity().getApplicationContext(), AddGoalActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        choiceDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "daily calendar", Toast.LENGTH_SHORT).show();
+
+                weeklyCalendar.setVisibility(View.GONE);
+                monthlyCalendar.setVisibility(View.GONE);
+            }
+        });
+
+        choiceWeek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "weekly calendar", Toast.LENGTH_SHORT).show();
+
+                weeklyCalendar.setVisibility(View.VISIBLE);
+                monthlyCalendar.setVisibility(View.GONE);
+            }
+        });
+
+        choiceMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "monthly calendar", Toast.LENGTH_SHORT).show();
+
+                weeklyCalendar.setVisibility(View.GONE);
+                monthlyCalendar.setVisibility(View.VISIBLE);
             }
         });
 
