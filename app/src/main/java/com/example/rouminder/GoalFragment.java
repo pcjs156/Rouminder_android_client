c66ccc29c6e96353e7e470667fbf15f325b008cd
윤민지 <gee2450@kookmin.ac.kr>
2021-10-19 14:45:58 +0900

    Add activity_add_goal, calendar and modify fragment_goal.xml goal fragment에서 plus button 누를 때 activity_add_goal로 이동 구현 goal fragment에 calendear 추가(현재 monthly는 visibility="gone"처리)

--- app/src/main/java/com/example/rouminder/GoalFragment.java
+++ app/src/main/java/com/example/rouminder/GoalFragment.java
@@ -1,5 +1,6 @@
 package com.example.rouminder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class GoalFragment extends Fragment {
+
+    ImageView btnAddGoal;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goal, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_goal, container, false);
+
+        btnAddGoal = (ImageView) rootView.findViewById(R.id.btnAddGoal);
+
+        btnAddGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "로그인", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getActivity().getApplicationContext(), AddGoalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 }
