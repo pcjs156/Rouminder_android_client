fcb0337c6279fa1ca30d012d2d2b252470a9cb6f
soominyeo <yeosm0108@kookmin.ac.kr>
2021-11-14 02:20:46 +0900

    modify GoalManager as it should be

--- app/src/main/java/com/example/rouminder/data/goalsystem/GoalManager.java
+++ app/src/main/java/com/example/rouminder/data/goalsystem/GoalManager.java
@@ -1,43 +1,197 @@
 package com.example.rouminder.data.goalsystem;
 
-import com.example.rouminder.data.goalsystem.action.Action;
-import com.example.rouminder.data.goalsystem.action.ActionInstance;
-import com.example.rouminder.data.goalsystem.condition.Condition;
-import com.example.rouminder.data.goalsystem.condition.ConditionInstance;
-import com.example.rouminder.data.goalsystem.goal.Goal;
-import com.example.rouminder.data.goalsystem.goal.GoalInstance;
+import androidx.annotation.Nullable;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.Vector;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class GoalManager {
-    private final Set<Goal> goals;
-    private final TreeSet<GoalInstance> earlyStartingActionInstanceSet;
-    private final TreeSet<GoalInstance> earlyEndingGoalInstanceSet;
-    private final Set<Action<?>> actions;
-    private final Set<ActionInstance<?>> actionInstances;
-    private final Set<Condition<?>> conditions;
-    private final Set<ConditionInstance<?>> conditionInstances;
+    private static final Period timeToExpire = Period.ofMonths(1);
+    private final HashMap<Integer, Goal> goals;
+    private final TreeSet<Goal> earlyStartingGoals;
+    private final TreeSet<Goal> earlyEndingGoals;
+    private final Vector<Goal> pendingGoals;
+    private final Set<Goal> ongoingGoals;
+    private final Set<Goal> endedGoals;
 
     public GoalManager() {
-        goals = new HashSet<>();
-        earlyStartingActionInstanceSet = new TreeSet<>(new Comparator<GoalInstance>() {
+        Set<Integer> a = new HashSet<>();
+        Goal goal;
+        goals = new HashMap<>();
+        earlyStartingGoals = new TreeSet<>(new Comparator<Goal>() {
             @Override
-            public int compare(GoalInstance g1, GoalInstance g2) {
+            public int compare(Goal g1, Goal g2) {
                 return g1.getStartTime().compareTo(g2.getStartTime());
             }
         });
-        earlyEndingGoalInstanceSet = new TreeSet<>(new Comparator<GoalInstance>() {
+        earlyEndingGoals = new TreeSet<>(new Comparator<Goal>() {
             @Override
-            public int compare(GoalInstance g1, GoalInstance g2) {
+            public int compare(Goal g1, Goal g2) {
                 return g1.getEndTime().compareTo(g2.getEndTime());
             }
         });
-        actions = new HashSet<>();
-        actionInstances = new HashSet<>();
-        conditions = new HashSet<>();
-        conditionInstances = new HashSet<>();
+        pendingGoals = new Vector<>();
+        ongoingGoals = new HashSet<>();
+        endedGoals = new HashSet<>();
+    }
+
+    /**
+     * Add a goal to the manager.
+     *
+     * @param goal a goal to be added.
+     * @return the id of the goal if succeed, otherwise -1.
+     */
+    public int addGoal(Goal goal) {
+        return -1;
+    }
+
+    /**
+     * Get a goal from the manager
+     *
+     * @param id an identifier of a goal to be found.
+     * @return a goal object if found, otherwise null.
+     */
+    public Goal getGoal(int id) {
+        return goals.getOrDefault(id, null);
+    }
+
+    /**
+     * Remove a goal from the manager
+     *
+     * @param id an identifier of a goal to be removed.
+     * @return true if successfully remove the goal, otherwise false.
+     */
+    public boolean removeGoal(int id) {
+        return false;
+    }
+
+    /**
+     * Get GoalInstances that matches domain and state.
+     *
+     * @param now    a LocalDateTime object of now.
+     * @param domain a Domain enum that is used to check if instance is the same in specific domain.
+     * @param status a current state that GoalInstance should have.
+     * @return a List of GoalInstances that matches criteria.
+     */
+    public List<Goal> getGoals(LocalDateTime now, @Nullable Domain domain, @Nullable Status status) {
+        if (domain == null)
+            domain = Domain.ALL;
+
+        if (status == null)
+            status = Status.ALL;
+
+        DomainFilter domainFilter = new DomainFilter(now, domain);
+        StatusFilter statusFilter = new StatusFilter(now, status);
+
+        Goal dummy = domainFilter.getDummy();
+        Goal bottom = earlyEndingGoals.floor(dummy);
+        Goal top = earlyStartingGoals.ceiling(dummy);
+        SortedSet<Goal> setFromBottom = earlyEndingGoals.tailSet(bottom);
+        SortedSet<Goal> setFromTop = earlyStartingGoals.headSet(top);
+        List<Goal> domainFiltered = setFromBottom.stream()
+                .filter(val -> !setFromTop.contains(val))
+                .collect(Collectors.toList());
+
+        List<Goal> statusFiltered = domainFiltered.stream().filter(statusFilter).collect(Collectors.toList());
+        return statusFiltered;
+    }
+
+    /**
+     * A enum for namespacing the domain in time, such as day, month, etc.
+     */
+    public enum Domain {
+        ALL,
+        DAY,
+        WEEK,
+        MONTH
+    }
+
+    /**
+     * A enum for namespacing the status of a goal, such as 'Before start'.
+     */
+    public enum Status {
+        ALL,
+        BEFORE,
+        ONGOING,
+        AFTER
+    }
+
+    private static class DomainFilter implements Predicate<Goal> {
+        private final LocalDateTime start;
+        private final LocalDateTime end;
+
+        public DomainFilter(LocalDateTime now, Domain domain) {
+            switch (domain) {
+                case DAY:
+                    start = now.truncatedTo(ChronoUnit.DAYS);
+                    end = start.plusDays(1);
+                    break;
+                case WEEK:
+                    start = now.truncatedTo(ChronoUnit.DAYS).with(DayOfWeek.MONDAY);
+                    end = start.plusDays(7);
+                    break;
+                case MONTH:
+                    start = now.truncatedTo(ChronoUnit.MONTHS);
+                    end = start.plusMonths(1);
+                    break;
+                case ALL:
+                default:
+                    start = null;
+                    end = null;
+                    break;
+            }
+        }
+
+        /**
+         * Create a dummy goal for effective search.
+         * @return a goal with given range
+         */
+        public Goal getDummy() {
+            return new Goal(start, end);
+        }
+
+        @Override
+        public boolean test(Goal goal) {
+            return (start == null && end == null)
+                    || (goal.getEndTime().isAfter(start) && goal.getStartTime().isBefore(end))
+                    ;
+        }
+    }
+
+    private static class StatusFilter implements Predicate<Goal> {
+        private final Status status;
+        private final LocalDateTime now;
+
+        public StatusFilter(LocalDateTime now, Status status) {
+            this.status = status;
+            this.now = now;
+        }
+
+        @Override
+        public boolean test(Goal goal) {
+            switch (status) {
+                case BEFORE:
+                    return goal.isBeforeStart(now);
+                case ONGOING:
+                    return goal.isOnProgress(now);
+                case AFTER:
+                    return goal.isAfterEnd(now);
+                case ALL:
+                default:
+                    return true;
+            }
+        }
     }
 }
