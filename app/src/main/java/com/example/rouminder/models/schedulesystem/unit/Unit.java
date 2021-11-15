98eda3f2bba7bec34d3edfed605bc1bd1af80817
soominyeo <yeosm0108@kookmin.ac.kr>
2021-11-16 01:50:47 +0900

    create an abstract class Unit, which is responsible for time units and make enum UnitDomain an inner enum for unit

---
+++ app/src/main/java/com/example/rouminder/models/schedulesystem/unit/Unit.java
@@ -0,0 +1,255 @@
+package com.example.rouminder.models.schedulesystem.unit;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.WeekFields;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeSet;
+
+public abstract class Unit {
+    protected final int bitmask;
+    protected final TreeSet<Domain> domains;
+    protected final Map<Domain, Integer> values;
+
+    protected Unit(List<Domain> domains) {
+        this.domains = new TreeSet<>(domains);
+        this.values = new HashMap<>();
+        int bitmask = 0;
+        for (Domain domain : this.domains) {
+            bitmask |= domain.getBitmask();
+            this.values.put(domain, domain.getStart());
+        }
+        this.bitmask = bitmask;
+    }
+
+    /**
+     * Check if a unit is compatible with another unit.
+     *
+     * @param unit a unit to check compatibility.
+     * @return true if a unit is compatible with another, otherwise false.
+     */
+    public boolean isCompatible(Unit unit) {
+        return (bitmask & unit.bitmask) == 0;
+    }
+
+    /**
+     * Set values of a unit at each domain.
+     *
+     * @param values a number of values in their domain's ascending order.
+     *               set null to skip the value in specific position.
+     */
+    public void set(Integer... values) {
+        Iterator<Domain> it = domains.iterator();
+        for (Integer value : values) {
+            if (!it.hasNext())
+                break;
+            if (value != null) {
+                this.values.put(it.next(), value);
+            }
+        }
+    }
+
+    /**
+     * Get values of a unit at each domain.
+     *
+     * @return a list of values in their domain's ascending order.
+     */
+    public List<Integer> get() {
+        List<Integer> values = new ArrayList<>();
+        for (Domain domain : domains) {
+            values.add(this.values.get(domain));
+        }
+        return values;
+    }
+
+    /**
+     * Apply a unit to the LocalDateTime object. Only should the part matched with unit's domain be replaced.
+     *
+     * @param time a LocalDateTime object to be replaced.
+     * @return a new LocalDateTime object with the unit applied.
+     */
+    public LocalDateTime apply(LocalDateTime time) {
+        LocalDateTime temp = time;
+        for (Domain domain : domains) {
+            temp = domain.apply(temp, values.getOrDefault(domain, 0));
+        }
+        return temp;
+    }
+
+    /**
+     * A domain, or primary time unit like minute, hour, etc., compromising time units.
+     * Each domain has its own bit for checking conflicts of multiple units.
+     */
+    public enum Domain implements Comparable<Domain> {
+        MINUTE("minute", 0x0001, 0, 60),
+        HOUR("hour", 0x0002, 0, 24),
+        DAY("day", 0x0004, 0, 7),
+        WEEK("week", 0x0008, 0, 4),
+        MONTH("month", 0x0010, 0, 12);
+
+        private static final int MAX_BITMASK;
+        private static final int MIN_BITMASK = 0x0000;
+        private static final DayOfWeek startDayOfWeek = DayOfWeek.MONDAY;
+
+        static {
+            MAX_BITMASK = 2 * values()[values().length - 1].bitmask - 1;
+        }
+
+        private final String id;
+        private final int bitmask;
+        /**
+         * range of unit per domain as integer.
+         * the range is half-open range; [start, end)
+         * -1 for undefined boundary
+         */
+        private final int start;
+        private final int end;
+
+        Domain(String id, int bitmask) {
+            this(id, bitmask, -1, -1);
+        }
+
+        Domain(String id, int bitmask, int start, int end) {
+            this.id = id;
+            this.bitmask = bitmask;
+            this.start = start;
+            this.end = end;
+        }
+
+        /**
+         * Calculate a final product of bit-or operation of multiple domains.
+         *
+         * @param domains a List of domains to calculate.
+         * @return a result bitmask.
+         */
+        public static int orBitmask(List<Domain> domains) {
+            int bitmask = MIN_BITMASK;
+            for (Domain domain : domains) {
+                bitmask |= domain.getBitmask();
+            }
+            return bitmask;
+        }
+
+        /**
+         * Calculate a final product of bit-and operation of multiple domains.
+         *
+         * @param domains a List of domains to calculate.
+         * @return a result bitmask.
+         */
+        public static int andBitmask(List<Domain> domains) {
+            int bitmask = MAX_BITMASK;
+            for (Domain domain : domains) {
+                bitmask &= domain.getBitmask();
+            }
+            return bitmask;
+        }
+
+        /**
+         * Get domains from bitmask.
+         *
+         * @param bitmask a bitmask.
+         * @return an array of Domain
+         */
+        public static List<Domain> getDomainsFromBitmask(int bitmask) {
+            int normalized = bitmask & MAX_BITMASK;
+            List<Domain> domains = new ArrayList<>();
+            for (Domain domain : Domain.values()) {
+                if ((normalized & domain.getBitmask()) != 0)
+                    domains.add(domain);
+            }
+            return domains;
+        }
+
+        /**
+         * Get a start value, or possible smallest value of the domain.
+         *
+         * @return a start value.
+         */
+        public int getStart() {
+            return start;
+        }
+
+        /**
+         * Get an end value, or possible largest value of the domain.
+         * It returns -1 if not defined.
+         *
+         * @return an end value, or -1 if end value is not defined.
+         */
+        public int getEnd() {
+            return end;
+        }
+
+        /**
+         * Overwrite a value at a domain to the LocalDateTime object.
+         *
+         * @param time  a LocalDateTime object to be replaced.
+         * @param value a value used to overwrite at the domain.
+         * @return a new LocalDateTime object with its value overwritten at the domain, or return null if value is out-of-bound.
+         */
+        public LocalDateTime apply(LocalDateTime time, int value) {
+            LocalDateTime newTime;
+            switch (this) {
+                case MINUTE:
+                    newTime = time.truncatedTo(ChronoUnit.MINUTES)
+                            .plusMinutes(value);
+                    break;
+                case HOUR:
+                    newTime = time.truncatedTo(ChronoUnit.HOURS)
+                            .plusHours(value)
+                            .plusMinutes(time.getMinute());
+                    break;
+                case DAY:
+                    newTime = time.with(TemporalAdjusters.previousOrSame(startDayOfWeek))
+                            .plusDays(value)
+                            .plusHours(time.getHour())
+                            .plusMinutes(time.getMinute());
+                    break;
+                case WEEK:
+                    newTime = time.with(TemporalAdjusters.firstDayOfMonth())
+                            .with(TemporalAdjusters.nextOrSame(startDayOfWeek))
+                            .with(TemporalAdjusters.nextOrSame(time.getDayOfWeek()))
+                            .plusHours(time.getHour())
+                            .plusMinutes(time.getMinute());
+                    break;
+                case MONTH:
+                    newTime = time.plusYears(value == 12 ? 1 : 0).withMonth((value + 1) % 12)
+                            .with(TemporalAdjusters.firstDayOfMonth())
+                            .with(TemporalAdjusters.nextOrSame(startDayOfWeek))
+                            .plusWeeks(time.get(WeekFields.of(Locale.getDefault()).weekOfMonth()) - 1)
+                            .with(TemporalAdjusters.nextOrSame(time.getDayOfWeek()))
+                            .plusHours(time.getHour())
+                            .plusMinutes(time.getMinute());
+                    break;
+                default:
+                    newTime = time;
+            }
+
+            return newTime;
+        }
+
+        /**
+         * Get an identifier of each domain.
+         *
+         * @return an identifier String.
+         */
+        public String getId() {
+            return id;
+        }
+
+        /**
+         * Get a bitmask of a domain.
+         *
+         * @return a bitmask.
+         */
+        public int getBitmask() {
+            return bitmask;
+        }
+    }
+}
