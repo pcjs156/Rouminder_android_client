d9c25e81ab335cb6e581273d8af060cc88a24b1c
soominyeo <yeosm0108@kookmin.ac.kr>
2021-11-24 20:38:28 +0900

    implement abstracts in Unit subclasses

--- app/src/main/java/com/example/rouminder/models/schedulesystem/unit/CompoundUnit.java
+++ app/src/main/java/com/example/rouminder/models/schedulesystem/unit/CompoundUnit.java
@@ -2,7 +2,34 @@ package com.example.rouminder.models.schedulesystem.unit;
 
 
 public class CompoundUnit extends Unit{
+    private final Domain from;
+    private final Domain to;
+
     public CompoundUnit(Domain from, Domain to) {
         super(Domain.getDomainsFromRange(from, to));
+        this.from = from;
+        this.to = to;
+    }
+
+    /**
+     * Get another unit object with identical domains from a unit.
+     *
+     * @return another unit with identical domains.
+     */
+    @Override
+    public Unit another() {
+        return new CompoundUnit(from, to);
+    }
+
+    /**
+     * Get a copy of a unit.
+     *
+     * @return a copy of a unit.
+     */
+    @Override
+    public Unit copy() {
+        CompoundUnit copy = new CompoundUnit(from, to);
+        copy.setValues(getValues().toArray(new Integer[0]));
+        return copy;
     }
 }
--- app/src/main/java/com/example/rouminder/models/schedulesystem/unit/SimpleUnit.java
+++ app/src/main/java/com/example/rouminder/models/schedulesystem/unit/SimpleUnit.java
@@ -3,20 +3,57 @@ package com.example.rouminder.models.schedulesystem.unit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class SimpleUnit extends Unit{
-
+public class SimpleUnit extends Unit {
+    private final Domain domain;
     private final String id;
 
     protected SimpleUnit(String id, Domain domain) {
         super(Stream.of(domain).collect(Collectors.toList()));
+        this.domain = domain;
         this.id = id;
     }
 
-    SimpleUnit(SimpleUnits simpleUnits) {
+    public SimpleUnit(SimpleUnits simpleUnits) {
         this(simpleUnits.id, simpleUnits.domain);
     }
 
+    /**
+     * Get an ID for a simple unit.
+     * e.g. "minute", "hour", ...
+     *
+     * @return an ID string.
+     */
     public String getId() {
         return id;
+    }
+
+    public int get() {
+        return getValues().get(0);
+    }
+
+    public void set(int value) {
+        setValues(value);
+    }
+
+    /**
+     * Get another unit object with identical domains from a unit.
+     *
+     * @return another unit with identical domains.
+     */
+    @Override
+    public Unit another() {
+        return new SimpleUnit(id, domain);
+    }
+
+    /**
+     * Get a copy of a unit.
+     *
+     * @return a copy of a unit.
+     */
+    @Override
+    public Unit copy() {
+        SimpleUnit copy = new SimpleUnit(id, domain);
+        copy.set(get());
+        return copy;
     }
 }
