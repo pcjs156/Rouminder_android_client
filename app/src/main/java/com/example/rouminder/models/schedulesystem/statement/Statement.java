225807d5b400de4f60bebabd218d5b2bf2c00971
soominyeo <yeosm0108@kookmin.ac.kr>
2021-11-24 20:39:31 +0900

    add abstract methods to Statement.java for statement-expression conversions

--- app/src/main/java/com/example/rouminder/models/schedulesystem/statement/Statement.java
+++ app/src/main/java/com/example/rouminder/models/schedulesystem/statement/Statement.java
@@ -2,6 +2,24 @@ package com.example.rouminder.models.schedulesystem.statement;
 
 import com.example.rouminder.models.schedulesystem.unit.Unit;
 
-public abstract class Statement<U extends Unit> {
+public abstract class Statement {
+    private Unit unit;
+
+    /**
+     * Get a statement from an expression.
+     *
+     * @param expression an String expression.
+     * @return a statement object.
+     */
+    public static Statement fromExpression(String expression) {
+        return null;
+    }
+
+    /**
+     * Get an expression from a statement.
+     *
+     * @return an String expression.
+     */
     public abstract String toExpression();
+
 }
