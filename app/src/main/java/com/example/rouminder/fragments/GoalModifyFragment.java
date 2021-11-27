d99a04acfa9d00bac4c5453c69f05406dda2a49a
0utDoorFr0g <63755945+0utDoorFr0g@users.noreply.github.com>
2021-11-27 17:52:07 +0900

    전체적인 개선
    날짜받아오는 중에 에러 발생 처리해야함.

--- app/src/main/java/com/example/rouminder/fragments/GoalModifyFragment.java
+++ app/src/main/java/com/example/rouminder/fragments/GoalModifyFragment.java
@@ -4,7 +4,6 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +13,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
-import com.example.rouminder.MainApplication;
 import com.example.rouminder.R;
 import com.example.rouminder.data.goalsystem.Goal;
 
@@ -29,87 +26,99 @@ import java.time.format.DateTimeFormatter;
 import javax.annotation.Nullable;
 
 public class GoalModifyFragment extends DialogFragment {
-    int goalID;
-    public GoalModifyFragment(Goal goal) { this.goalID = goal.getId(); }
-    @NonNull
+    Goal goal;
+    public GoalModifyFragment(Goal goal) {this.goal = goal;}
+
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_goal_modify, container);
-        EditText editTextGoalName = v.findViewById(R.id.editTextGoalName);
-        Button buttonConfirm = v.findViewById(R.id.buttonConfirm);
-        Button buttonCancel = v.findViewById(R.id.buttonCancel);
-        TextView textViewGoalStartTimeData = v.findViewById(R.id.textViewGoalStartTimeDataFront);
-        TextView textViewGoalEndTimeData = v.findViewById(R.id.textViewGoalEndTimeDataFront);
-
-        Goal goal = ((MainApplication)getActivity().getApplication()).getGoalManager().getGoal(goalID);
-        editTextGoalName.setText(goal.getName());
-        textViewGoalStartTimeData.setText(goal.getStartTime().toString());
-        textViewGoalEndTimeData.setText(goal.getEndTime().toString());
+        View view = inflater.inflate(R.layout.fragment_goal_modify, container);
+        Button buttonGoalCancel = view.findViewById(R.id.buttonGoalCancel); // 취소 버튼
+        Button buttonGoalChange = view.findViewById(R.id.buttonGoalModify); // 수정 버튼
+        TextView textViewStartTimeFront = view.findViewById(R.id.textViewStartTimeFront); // 시작 시간, 년월일
+        TextView textViewStartTimeBack = view.findViewById(R.id.textViewStartTimeBack); // 시작 시간, 시분
+        TextView textViewEndTimeFront = view.findViewById(R.id.textViewEndTimeFront); // 마감 시간, 년월일
+        TextView textViewEndTimeBack = view.findViewById(R.id.textViewEndTimeBack); // 마감 시간, 시분
+        EditText editTextGoalName = view.findViewById(R.id.editTextGoalName); // 골 이름
 
         Context context = getContext();
+        LocalDateTime startTime = goal.getStartTime();
+        LocalDateTime endTime = goal.getEndTime();
 
-        textViewGoalStartTimeData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("GoalModifyFragment", "StartTime Clicked");
-                LocalDateTime cur = LocalDateTime.now();
-                DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
-                        resetDate(view, year, monthOfYear + 1, dayOfMonth);
-                    }
-                };
-                TimePickerDialog.OnTimeSetListener timeListener = new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-                        resetTime(view, hour, minute);
-                    }
-                };
-                DatePickerDialog dialog = new DatePickerDialog(context, dateListener,
-                        cur.getYear(), cur.getMonthValue() - 1, cur.getDayOfMonth());
-                dialog.show();
-            }
-        });
+        editTextGoalName.setText(goal.getName());
 
-        textViewGoalEndTimeData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("GoalModifyFragment", "EndTime Clicked");
-                LocalDateTime cur = goal.getEndTime();
-                DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
-                        resetDate(view, year, monthOfYear + 1, dayOfMonth);
-                    }
-                };
-                TimePickerDialog.OnTimeSetListener timeListener = new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-                        resetTime(view, hour, minute);
-                    }
-                };
-                DatePickerDialog dialog = new DatePickerDialog(context, dateListener,
-                        cur.getYear(), cur.getMonthValue() - 1, cur.getDayOfMonth());
-                dialog.show();
-            }
-        });
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy:MM:dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm");
 
+        textViewStartTimeFront.setText(goal.getStartTime().format(dateFormatter));
+        textViewStartTimeBack.setText(goal.getStartTime().format(timeFormatter));
 
-        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+        textViewEndTimeFront.setText(goal.getEndTime().format(dateFormatter));
+        textViewEndTimeBack.setText(goal.getEndTime().format(timeFormatter));
+
+        buttonGoalCancel.setOnClickListener(new View.OnClickListener() { // 취소 버튼 클릭 리스너
             @Override
             public void onClick(View view) {
                 dismiss();
             }
         });
 
-        buttonCancel.setOnClickListener(new View.OnClickListener() {
+        buttonGoalChange.setOnClickListener(new View.OnClickListener() { // 수정 버튼 클릭 리스너
             @Override
             public void onClick(View view) {
+                // GoalManager를 이용하여 데이터 수정하도록 만들기.
                 dismiss();
             }
         });
 
-        return v;
+        DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() { // 년월일 받는 리스너
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                resetDate(view, year, monthOfYear + 1, dayOfMonth);
+            }
+        };
+
+        TimePickerDialog.OnTimeSetListener timeListener = new TimePickerDialog.OnTimeSetListener() { // 시분 받는 리스너
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                resetTime(view, hour, minute);
+            }
+        };
+
+        textViewStartTimeFront.setOnClickListener(new View.OnClickListener() { // 시작 시간, 년월일
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog dialog= new DatePickerDialog(context, dateListener, startTime.getYear(), startTime.getMonthValue() - 1, startTime.getDayOfMonth());
+                dialog.show();
+            }
+        });
+
+        textViewStartTimeBack.setOnClickListener(new View.OnClickListener() { // 시작 시간, 시분
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog dialog = new TimePickerDialog(context, timeListener, startTime.getHour(), startTime.getMinute(), false);
+                dialog.show();
+            }
+        });
+
+        textViewEndTimeFront.setOnClickListener(new View.OnClickListener() { // 마감 시간, 년월일
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog dialog= new DatePickerDialog(context, dateListener, endTime.getYear(), endTime.getMonthValue() - 1, endTime.getDayOfMonth());
+                dialog.show();
+            }
+        });
+
+        textViewEndTimeBack.setOnClickListener(new View.OnClickListener() { // 마감 시간, 시분
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog dialog = new TimePickerDialog(context, timeListener, endTime.getHour(), endTime.getMinute(), false);
+                dialog.show();
+            }
+        });
+
+        setCancelable(false);
+        return view;
     }
 
     private void resetDate(View view, int year, int month, int day) {
@@ -129,4 +138,5 @@ public class GoalModifyFragment extends DialogFragment {
         TextView editText = (TextView) view;
         editText.setText(strDate);
     }
+
 }
